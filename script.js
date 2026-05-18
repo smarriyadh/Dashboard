8d13f1e3b43987c74c846688764e8b3a6ff9ff30
smarriyadh <smarriyadh@gmail.com>
2026-05-18 19:09:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // script.js
 // REPLACE THIS WITH YOUR ACTUAL APPS SCRIPT WEB APP URL
-const API_URL = 'https://script.google.com/macros/s/AKfycbwppm7qS0jkQjXe3QHiLfWxdl8HSk1RRmm0T3P0SkrTcNziVohe60oLKDRGWhLgDnw/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzC6JnYAldqWLv3qDrvyRwV0rTzcDAvI6f0YlWBwHh1VIOl7_f_lMp272psZPZvHnQ/exec';
 let allContracts = [];
 let amountByProfessionChart, contractsByNationalityChart, amountOverTimeChart;
 
